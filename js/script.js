0caf3f89ffadefb2c8f5c05c184dae84a9e79e20
Vano-P <93191667+Vano-P@users.noreply.github.com>
2022-10-29 01:22:36 +0400

    update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-let fBtn = document.querySelector("#fBtn");
+let fName = document.querySelector('#fName');
+let fReq = document.querySelector('#fReq');
+
+let wrongRes = document.querySelector('#wrongRes');
+
+if (fName.value === '' || '' || '' || '') {
+  fReq.style.display = 'block';
+} else {
+  
+}
+
+/*let fBtn = document.querySelector("#fBtn");
 fBtn.addEventListener("click", () => {
   fBtn.style.display = 'none';
-});
+});*/
